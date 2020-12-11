57fa86906d18464e3f44bb590326f68920f346b8
Christian Menzel <info@chrmenzel.de>
2020-12-11 21:43:28 +0100

    display the images with the original

--- CombiningThresholds.py
+++ CombiningThresholds.py
@@ -98,12 +98,62 @@ grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100)
 mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
 dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
 
-# Plot the result
+# Plot the results: dir_binary
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
 ax2.imshow(dir_binary, cmap='gray')
 ax2.set_title('Thresholded Grad. Dir.', fontsize=50)
-# plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
+
+# Plot the results: mag_binary
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(mag_binary, cmap='gray')
+ax2.set_title('Thresholded Magnitude', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
+
+# Plot the results: gradx
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(gradx, cmap='gray')
+ax2.set_title('Thresholded x-derivative', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
+
+# Plot the results: grady
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(grady, cmap='gray')
+ax2.set_title('Thresholded y-derivative', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
+
+"""
+combine combinations, e. g. x and y gradients meet the threshold criteria or the gradient magnitude
+and direction are both within their threshold values.
+"""
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(combined, cmap='gray')
+ax2.set_title('Thresholded x and y-derivative or gradient magnitude', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
